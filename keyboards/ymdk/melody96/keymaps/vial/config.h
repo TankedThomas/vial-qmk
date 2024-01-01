d9d3d8431db3f8b677c4bb2aade2c0cbcfebc119
TankedThomas <TankedThomas@users.noreply.github.com>
2024-01-02 00:09:44 +1300

    Add vial support

---
+++ keyboards/ymdk/melody96/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x82, 0x56, 0xFE, 0xAF, 0x76, 0x1A, 0x2C, 0xA8}
+// limit number of layers to reduce firmware size
+// default layer numbers is 4
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
--- keyboards/ymdk/melody96/keymaps/vial/rules.mk
+++ keyboards/ymdk/melody96/keymaps/vial/rules.mk
@@ -1,2 +1,10 @@
-VIA_ENABLE = yes
 LTO_ENABLE = yes
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIAL_INSECURE = yes
+# the following are required to reduce the firmware size
+TAP_DANCE_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
+COMBO_ENABLE = no
+# the following options can be disabled instead of/as well as the above options
+# QMK_SETTINGS = no
